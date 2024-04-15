a543d7abd5cfddb7fd9a9201cf3c7fce41c4d6f7
David Salazar <david.salazar.-nd@disney.com>
2024-04-15 12:10:20 -0500

    hero page detail with redirect heroes list

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { enviroments } from 'enviroments/enviroments';
 import { Hero } from '../interfaces/hero.interface';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
+	private baseUrl: string = enviroments.baseUrl;
 
-    private baseUrl: string = enviroments.baseUrl;
+	constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) { }
+	getHeroes(): Observable<Hero[]> {
+		return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+	}
 
-    getHeroes(): Observable<Hero[]> {
-
-        return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
-
-    }
-
+	getHeroById(id: string): Observable<Hero | undefined> {
+		return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+			.pipe(
+				catchError( error => of(undefined) )
+			);
+	}
 }
